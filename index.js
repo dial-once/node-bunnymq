c51c820a045b98bf7bd7dd84d57a00b3bc330c04
Yacine Khatal <khatal.yacine@gmail.com>
2016-01-21 02:58:01 +0100

    Add config var

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
+// var uuid = require('node-uuid');
+
 var defaultConfig = {
   isLogEnabled: true,
   amqpUrl: process.env.AMQP_URL || 'amqp://localhost',
   prefetch: process.env.AMQP_PREFETCH || 1,
-  isRequeueEnabled: true
+  isRequeueEnabled: true,
+  queues: [{ name: '', options: { exclusive: true, persistent: true } }],
+  exchanges: [{ name: 'defaultExchange', type: 'topic', options: { durable: true, persistent: true } }],
+  bindings: [{ exchange: 'defaultExchange', queue: '', routingKeys: ['test.', '*.key'] }]
 };
 
 module.exports = function(config) {
