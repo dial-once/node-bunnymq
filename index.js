2a8f476a975003af4563d5c10c22f26fd3c6b155
Jonathan <jmuller@dial-once.com>
2016-02-25 13:43:21 +0100

    Refactor index and fix bad prefetch property parsing Fix #14

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+require('./lib/boot/logger');
 
 var defaultConfig = {
-  amqpUrl: process.env.AMQP_URL,
+  amqpUrl: 'amqp://admin:dialonce@rabbitmq-master.dialonce-europe-dev.dialonce.svc.tutum.io',
   prefetch: process.env.AMQP_PREFETCH || 1,
   isRequeueEnabled: true
 };
 
+if (typeof defaultConfig.prefetch !== 'number') {
+  console.log(defaultConfig);
+  defaultConfig.prefetch = parseInt(defaultConfig.prefetch);
+}
+
 module.exports = function(config) {
-  require('./lib/boot/logger');
-
-  if (typeof defaultConfig.prefetch !== 'number') {
-    defaultConfig.prefetch = parseInt(prefetch);
-  }
-
   return {
     producer: require('./lib/producer')(config || defaultConfig),
     consumer: require('./lib/consumer')(config || defaultConfig)
