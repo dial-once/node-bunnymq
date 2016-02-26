9e516457f0dfc7ce14eb0e2cfad05b89b5673e1f
Yacine Khatal <khatal.yacine@gmail.com>
2016-02-26 18:52:54 +0100

    Update example

--- examples/hello-world/consumer.js
+++ examples/hello-world/consumer.js
@@ -3,11 +3,6 @@ var consumer = require('../../index')().consumer;
 consumer.connect()
 .then(function (_channel) {
   consumer.consume('queueName', function (_msg) {
-    throw Error('Any kind of error');
-    // return new Promise(function (resolve, reject) {
-    //   setTimeout(function () {
-    //     resolve(true);
-    //   }, 5000);
-    // });
+    return true;
   });
 });
